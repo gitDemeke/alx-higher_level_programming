de9e28b052588a7223e3ef8e05c992d8b38b2c2a
gitDemeke <demeke.asres@aau.edu.et>
2023-04-13 19:56:36 +0300

    python network 1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Fetches the URL: https://intranet.hbtn.io/status
-with `requests` module
 """
-
+script that fetches https://alx-intranet.hbtn.io/status.
+"""
 import requests
 
 
 if __name__ == "__main__":
-    req = requests.get('https://intranet.hbtn.io/status')
-
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    content = response.text
     print('Body response:')
-    print('\t- type: {_type}'.format(_type=type(req.text)))
-    print('\t- content: {_content}'.format(_content=req.text))
+    print('\t- type: {}'.format(type(content)))
+    print('\t- content: {}'.format(content))
