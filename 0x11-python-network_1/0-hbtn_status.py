815902a1929fc6a9f96e9f4a4052d27c8d9fb223
gitDemeke <demeke.asres@aau.edu.et>
2023-04-13 19:41:40 +0300

    python network 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-"""Fetches the URL: https://intranet.hbtn.io/status
-"""
-
-from urllib.request import Request, urlopen
+"Module that fetches https://alx-intranet.hbtn.io/status"
+import urllib.request
 
 
 if __name__ == "__main__":
-    req = Request('https://intranet.hbtn.io/status')
-
-    with urlopen(req) as res:
-        content = res.read()
-        utf8_content = content.decode('utf-8')
-
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
         print('Body response:')
-        print('\t- type: {_type}'.format(_type=type(content)))
-        print('\t- content: {_content}'.format(_content=content))
-        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=utf8_content))
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
